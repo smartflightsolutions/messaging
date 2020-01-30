d60756f5239708fd13fdd407e8f18c64a99a0f1d
Taynan Souza <taynan.tws@gmail.com>
2020-01-30 15:18:38 -0300

    (chore) Change PubSub Implementation
    Changed Publish Method, now include timeout.

--- type.go
+++ type.go
@@ -2,31 +2,18 @@ package messaging
 
 import (
 	"cloud.google.com/go/pubsub"
-	"context"
+	"log"
 )
 
-// messageBroker provides an interface so we can swap out the
-// implementation of SendEmail under tests.
-type messageBroker interface {
-	send(ctx context.Context, topicID string, msg interface{}) error
-	receive(ctx context.Context, topicID string, subID string, c chan<- ResultMessage)
+// GCPubSub Client and topic
+type GCPubSub struct {
+	pubsubClient *pubsub.Client
+	pubsubTopic  *pubsub.Topic
+	Options
 }
 
-// GooglePubSub message broker
-type GooglePubSub struct {
-	projectID    string
-	client       *pubsub.Client
-	topic        *pubsub.Topic
-	subscription *pubsub.Subscription
-}
-
-// MessageAPI standard message api
-type MessageAPI struct {
-	API messageBroker
-}
-
-// ResultMessage type to communicate through a channel
-type ResultMessage struct {
-	Data []byte
-	Err  error
+// Options contains project id
+type Options struct {
+	ProjectID string
+	Logger    *log.Logger
 }
